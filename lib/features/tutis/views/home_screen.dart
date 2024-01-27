bdd8055ebf8c1268c031e5b30e35db7a8c3631cd
youksimgyu <dbrtlarb@gmail.com>
2024-01-27 22:20:21 +0900

    오타 수정

--- lib/features/tutis/views/home_screen.dart
+++ lib/features/tutis/views/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tuti/features/tutis/widgets/home_widgets/content_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpodets/content_widget.dart';
 import 'package:tuti/features/tutis/widgets/home_widgets/footer_widget.dart';
 
 class HomeScreen extends ConsumerWidget {
